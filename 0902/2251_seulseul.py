eac68be5e258ac4ace6c81fd4eb19fac81843f8e
조영훈 <gcloud1025@gmail.com>
2022-09-06 22:53:16 +0900

    0906 seulseul

--- 0902/2251_seulseul.py
+++ 0902/2251_seulseul.py
@@ -1,45 +1,18 @@
-from collections import deque
-import sys
-input = sys.stdin.readline
+def draw_stars(n):
+  if n==1:
+    return ['*']
 
-visited = [[False] * 201 for _ in range(201)]
-A, B, C = map(int, input().split())
+  Stars=draw_stars(n//3)
+  L=[]
 
-q = deque([[0, 0]])
-visited[0][0] = True
-lst = []
+  for star in Stars:
+    L.append(star*3)
+  for star in Stars:
+    L.append(star+' '*(n//3)+star)
+  for star in Stars:
+    L.append(star*3)
 
-while q:
-    a, b = q.popleft()
-    c = C-a-b
-    if a==0:
-        lst.append(c)
-    
-    # [붓는 물]과 [남은 용량] 중 작은 값을 더해야 한다.
-    tmp = min(a, B-b)
-    if not visited[a-tmp][b+tmp]:
-        visited[a-tmp][b+tmp] = True
-        q.append([a-tmp, b+tmp])
-    tmp = min(a, C-c)
-    if not visited[a-tmp][b]:
-        visited[a-tmp][b] = True
-        q.append([a-tmp, b])
-    tmp = min(b, A-a)
-    if not visited[a+tmp][b-tmp]:
-        visited[a+tmp][b-tmp] = True
-        q.append([a+tmp, b-tmp])
-    tmp = min(b, C-c)
-    if not visited[a][b-tmp]:
-        visited[a][b-tmp] = True
-        q.append([a, b-tmp])
-    tmp = min(c, A-a)
-    if not visited[a+tmp][b]:
-        visited[a+tmp][b] = True
-        q.append([a+tmp, b])
-    tmp = min(c, B-b)
-    if not visited[a][b+tmp]:
-        visited[a][b+tmp] = True
-        q.append([a, b+tmp])
+  return L
 
-lst.sort()
-print(*lst)
+N=int(input())
+print('\n'.join(draw_stars(N)))
